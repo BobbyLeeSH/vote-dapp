023a81ae978e39d2477f97c24a75896d5a5d9777
BobbyLeeSH <tkdgus1797@gmail.com>
2022-05-01 19:35:57 +0900

    truffle local env setup

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -1,13 +1,14 @@
-// SPDX-License-Identifier: GPL-3.0
+// SPDX-License-Identifier: MIT
 
 pragma solidity >=0.8.0 <0.9.0;
 
 contract Voting {
     string[] public candidates;
-    mapping (string => uint8) public votes;
+    mapping(string => uint8) public votes;
+
     // TODO. change it to onlyOnwer
     function registerCandidate(string memory candidate) public {
-        require(isExistingCandidate(candidate)==false);
+        require(isExistingCandidate(candidate) == false);
         candidates.push(candidate);
     }
 
@@ -16,9 +17,16 @@ contract Voting {
         votes[candidate] += 1;
     }
 
-    function isExistingCandidate(string memory candidate) view private returns (bool) {
-        for (uint i=0; i<candidates.length; i++) {
-            if (keccak256(abi.encodePacked(candidates[i])) == keccak256(abi.encodePacked(candidate))) {
+    function isExistingCandidate(string memory candidate)
+        private
+        view
+        returns (bool)
+    {
+        for (uint256 i = 0; i < candidates.length; i++) {
+            if (
+                keccak256(abi.encodePacked(candidates[i])) ==
+                keccak256(abi.encodePacked(candidate))
+            ) {
                 return true;
             }
         }
